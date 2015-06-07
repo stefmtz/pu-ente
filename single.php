75dddf99b0ac6125e04eca9ff52bee6929733500
Stef M <Stef@Stef.box>
2015-06-07 19:04:51 +0200

    Slide effect in galleries

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 <!-- single.php -->
-	<div id="primary" class="content-area project">
+	<div id="primary" class="content-area single-project">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
---
+++ source/javascript/controllers/gallery-controller.js
@@ -0,0 +1,49 @@
+/* Gallery Controller*/
+
+var GalleryController = function () {
+
+	var _photos = null;
+	var _currentPhoto;
+	var _nIntervId;
+
+	var init = function () {
+		
+		var galleries = document.getElementsByClassName("gallery");
+
+		for (var i = 0; i < galleries.length; i++) {
+			_initGallery(galleries[i]);
+		}
+	};
+
+	var _initGallery = function(gallery){
+
+		_photos = document.getElementsByClassName("gallery-item");
+
+		_currentPhoto = _photos.length-1;
+		_nIntervId = setInterval(_fadePhoto, 3000);
+
+	};
+
+	var _fadePhoto = function(){
+
+		if (_currentPhoto!=0){
+			_photos[_currentPhoto].className = _photos[_currentPhoto].className + " fade-out";
+		} else {
+			/*STEF MEJORAR: se puede mejorar haciendo fade-in primero de la primera foto y luego del resto */	
+			for (var i = 0; i < _photos.length; i++) {
+				_photos[i].className = _photos[i].className.replace(" fade-out", "");
+			};			
+		}
+
+		_currentPhoto == 0 ? _currentPhoto = _photos.length-1 : _currentPhoto--;
+
+	}
+
+
+	return {
+		init: init
+	};
+
+};
+
+module.exports = new GalleryController();
