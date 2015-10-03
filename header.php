29b6db3271c5d3621e4ae441a44842b43fb80296
Stef M <Stef@Stef.home>
2015-10-03 18:20:51 +0200

    Add Google Analytics

--- header.php
+++ header.php
@@ -17,6 +17,16 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/public/css/default.css" />  
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/public/js/main.js" defer></script>
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54570397-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 
 <body <?php 
